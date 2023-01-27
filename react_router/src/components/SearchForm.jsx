fda13f03c95faf7ac1f5cd914125543a191c28f0
IsaqueP <isaquepinheiro2002@gmail.com>
2023-01-27 11:30:23 -0300

    add react_router

---
+++ react_router/src/components/SearchForm.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+export function SearchForm(){
+  const navigate = useNavigate()
+  const [ query, setQuery ] = useState()
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    navigate("/search?q=" + query)
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={(e) => setQuery(e.target.value)} />
+      <input type="submit" value="Buscar" />
+
+    </form>
+  )
+}
---
+++ react_router/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function NotFound(){
+
+
+  return(
+    <div>
+      <h1>404</h1>
+      <p>Página não encontrada!!!</p>
+    </div>
+  )
+}
