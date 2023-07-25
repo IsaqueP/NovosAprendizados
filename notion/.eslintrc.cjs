51987b7547df577b4b2ce10113923da937832fa5
IsaqueP <isaquepinheiro2002@gmail.com>
2023-07-25 20:49:44 -0300

    add notion

---
+++ notion/.eslintrc.cjs
@@ -0,0 +1,29 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  files: ['*.ts', '*.tsx'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+  ],
+  
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+}
---
+++ notion/src/components/initialContent.ts
@@ -0,0 +1,40 @@
+export const initialContent = `
+<h1>Comparando Tailwind CSS e CSS Modules</h1>
+
+      <p>Bem-vindo ao nosso blog sobre Tailwind CSS ou CSS Modules! Neste post, vamos explorar as diferenças entre essas duas abordagens populares para estilização de componentes em aplicações web.</p>
+
+      <h2>O que é o Tailwind CSS?</h2>
+
+      <p>Voce pode aprender mais em <a href="https://www.rocketseat.com.br/discover">rocketseat</a></p>
+
+      <p>O Tailwind CSS é uma estrutura de design CSS altamente configurável e de baixo nível. Ele fornece uma série de classes utilitárias pré-definidas que você pode usar para estilizar rapidamente seus componentes. Com o Tailwind CSS, você pode criar designs exclusivos de forma eficiente e consistente.</p>
+
+      <h2>O que são CSS Modules?</h2>
+
+      <p>Os CSS Modules são uma abordagem para estilização em que os estilos são escopados aos componentes. Cada arquivo CSS é tratado como um módulo isolado, evitando conflitos de nomenclatura e permitindo que você use nomes de classe sem se preocupar com colisões em todo o seu projeto. Os CSS Modules ajudam a manter seus estilos organizados e evitam efeitos colaterais indesejados.</p>
+
+      <h2>Exemplo de uso do Tailwind CSS</h2>
+
+      <p>Aqui está um exemplo de como você pode estilizar um botão usando o Tailwind CSS:</p>
+
+      <pre><code class="language-javascript">console.log('teste')</code></pre>
+
+      <p>Neste exemplo, usamos as classes utilitárias do Tailwind CSS para definir a cor de fundo, a cor de hover, a cor do texto, a fonte, o espaçamento interno e o raio dos cantos do botão. Essas classes são reutilizáveis e podem ser combinadas para criar estilos personalizados.</p>
+
+      <h2>Exemplo de uso do CSS Modules</h2>
+
+      <p>Aqui está um exemplo de como você pode estilizar um botão usando CSS Modules:</p>
+
+      <pre><code class="language-javascript">let name = 'isaque'<br>console.log(name)</code></pre>
+
+      <p>No arquivo CSS correspondente:</p>
+
+      <p>Neste exemplo, definimos uma classe chamada "button" no arquivo CSS. Essa classe é aplicada ao botão no arquivo HTML correspondente. Os estilos definidos nesse arquivo CSS são aplicados apenas ao componente específico, evitando conflitos com outros estilos em seu projeto.</p>
+
+      <h2>Conclusão</h2>
+
+      <p>O Tailwind CSS e os CSS Modules são duas abordagens diferentes para estilização de componentes em aplicações web. O Tailwind CSS oferece uma abordagem utilitária e configurável, enquanto os CSS Modules permitem o escopo de estilos por componente. A escolha entre eles depende das necessidades e preferências do seu projeto. Ambas as abordagens têm seus pontos fortes e podem ser usadas com sucesso em diferentes contextos.</p>
+
+      <p>Esperamos que este post tenha fornecido uma visão geral das diferenças entre Tailwind CSS e CSS Modules. Sinta-se à vontade para explorar mais sobre essas abordagens e decidir qual é a melhor opção para o seu projeto!</p>
+
+`
